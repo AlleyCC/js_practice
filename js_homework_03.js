a15133902d2d885930a6797550048a7fef2cf87d
AlleyChang <yellaatthebeach@gmail.com>
2025-01-08 18:50:07 +0800

    [新增]陣列三練習

--- js_homework_03.js
+++ js_homework_03.js
@@ -12,8 +12,29 @@ const comments = [
   { text: 'Ramen is my fav food ever', id: 123523 },
   { text: 'Nice Nice Nice!', id: 542328 }
 ];
+var date = new Date(); 
+var currYear = date.getFullYear();
 //1.是否至少有一個人年滿19歲？
+function isAgeQualified(age, people){
+  return people.some(person => (currYear - person.year) >= age);
+}
+console.log(isAgeQualified(19, people));
 //2.是否所有人都年滿19歲？
+function isAllAgeQualified(age, people){
+  return filteredPeople = people.filter(person => (currYear - person.year) >= age);
+}
+console.log(isAllAgeQualified(19, people));
 //1.找出 ID 為823423的評論
+function searchComment(id, comments){
+  const filteredComment = comments.filter(comment => comment.id == id);
+  if (filteredComment.length < 1) return alert('no data matches');
+  return filteredComment;
+}
+console.log(searchComment(823423, comments));
+
 //2.刪除 ID 為823423的評論
+function deleteComment(id, comments){
+  return filteredComments = comments.filter(comment => comment.id !== id);
+}
+console.log(deleteComment(823423, comments));
 
